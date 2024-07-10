02ad61f142364f8daf48e99f2384992516680370
masaki yukina <56630518+y-masaki03@users.noreply.github.com>
2024-07-10 21:57:22 +0900

    chore: add @mdi/font

--- application/resources/js/app.js
+++ application/resources/js/app.js
@@ -4,6 +4,7 @@ import "vuetify/styles";
 import App from "./App.vue";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import '@mdi/font/css/materialdesignicons.css'
 import router from "./router";
 
 const vuetify = createVuetify({
--- application/resources/js/components/common/Header.vue
+++ application/resources/js/components/common/Header.vue
@@ -1,22 +1,33 @@
 <template>
-    <v-app-bar color="teal-darken-4" dark app>
-        <template v-slot:prepend>
-            <v-icon aria-hidden="false"> {{ home.icon }} </v-icon>
-        </template>
-        <template v-slot:title>
-            <v-app-bar-title>{{ title }}</v-app-bar-title>
-        </template>
-    </v-app-bar>
+  <v-app-bar color="teal-darken-4" dark app>
+    <template v-slot:prepend>
+      <v-icon aria-hidden="false"> {{ home.icon }} </v-icon>
+    </template>
+    <template v-slot:title>
+      <v-app-bar-title>{{ title }}</v-app-bar-title>
+    </template>
+    <v-btn
+      v-for="link in links"
+      :key="link"
+      class="mx-2"
+      color="white"
+      rounded="xl"
+      variant="text"
+    >
+      {{ link }}
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        title: "About Me",
-        home: {
-            icon: "mdi-home",
-            link: "", //今回は設定しない
-        },
-    }),
+  data: () => ({
+    title: "About Me",
+    home: {
+      icon: "mdi-home",
+      link: "",
+    },
+    links: ["Home", "About Me", "Skill Set", "Experience", "Product", "News"],
+  }),
 };
 </script>
