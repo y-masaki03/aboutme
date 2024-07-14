fb10e527641c61572e64a3007ab429bfcd5d6882
masaki yukina <56630518+y-masaki03@users.noreply.github.com>
2024-07-14 15:21:18 +0900

    feat: add page 404

---
+++ application/resources/js/pages/NotFound.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <v-container>
+    <v-row align-content="center" style="height: 500px">
+      <v-col cols="12" md="12" align="center">
+        <p class="mb-2 text-h4 font-weight-bold">
+          Sorry, the page you're looking for can't be found.
+        </p>
+        <p class="mb-2 text-subtitle-1">お探しのページが見つかりませんでした</p>
+      </v-col>
+      <v-col align="center">
+        <v-btn @click="goHome">
+          <v-icon class="mr-1"> mdi-home </v-icon>
+          Home
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    goHome() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
--- application/resources/js/router/index.js
+++ application/resources/js/router/index.js
@@ -1,15 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../pages/Home.vue';
-import About from '../pages/About.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import NotFound from "../pages/NotFound.vue";
+import Home from "../pages/Home.vue";
+import About from "../pages/About.vue";
+import SkillSet from "../pages/SkillSet.vue";
+import Experience from "../pages/Experience.vue";
 
 const routes = [
-  { path: '/', name:'home', component: Home },
-  { path: '/about', name: 'about', component: About },
+    { path: "/", name: "home", component: Home },
+    { path: "/about", name: "about", component: About },
+    { path: "/skillset", name: "skillset", component: SkillSet },
+    { path: "/experience", name: "experience", component: Experience },
+    { path: '/:matchAll(.*)', component: NotFound },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
