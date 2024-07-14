090f184d3dff6a460dfd73a419243c01da161d65
masaki yukina <56630518+y-masaki03@users.noreply.github.com>
2024-07-14 19:52:38 +0900

    feat: create view profile

---
+++ application/resources/js/components/Home/Profile.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-container id="aboutme">
+      <v-card variant="text">
+        <template v-slot:title>
+          <span class="font-weight-black">{{ title }}</span>
+        </template>
+        <v-container class="pt-0">
+          <v-list-item v-for="item in profile" :key="item" :title="item.text">
+            <template v-slot:prepend>
+              <p style="width: 150px">{{ item.title }}</p>
+            </template>
+          </v-list-item>
+        </v-container>
+      </v-card>
+    </v-container>
+    <v-divider class="border-opacity-50"></v-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: "Profile",
+    profile: [
+      {
+        title: "Name",
+        text: "眞木 雪菜（Masaki Yukina）",
+      },
+      {
+        title: "Speciality",
+        text: "フロントエンド, サーバーサイド",
+      },
+      {
+        title: "My Favorite",
+        text: "Chloe Moretz（女優）、Sherlock Holmes（映画/ドラマ）、There Is No Game（ゲーム）、Pentatonix（音楽）",
+      },
+    ],
+  }),
+};
+</script>
--- application/resources/js/pages/Home.vue
+++ application/resources/js/pages/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
-    <AboutMe />
+    <p class="text-h3 mb-1">{{ title }}</p>
+    <Profile />
     <SkillSet />
     <Experience />
     <ReleaseNote />
@@ -8,17 +9,20 @@
 </template>
 
 <script>
-import AboutMe from "../components/Home/AboutMe.vue";
+import Profile from "../components/Home/Profile.vue";
 import SkillSet from "../components/Home/SkillSet.vue";
 import Experience from "../components/Home/Experience.vue";
 import ReleaseNote from "../components/Home/ReleaseNote.vue";
 
 export default {
   components: {
-    AboutMe,
+    Profile,
     SkillSet,
     Experience,
     ReleaseNote,
   },
+  data: () => ({
+    title: "Thank you for visiting my website!",
+  }),
 };
 </script>
