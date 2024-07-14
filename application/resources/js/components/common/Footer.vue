c2c5cd9a3164727bb9220b8f67858ee467ad559e
masaki yukina <56630518+y-masaki03@users.noreply.github.com>
2024-07-14 20:35:19 +0900

    feat: change view footer

--- application/resources/js/components/common/Footer.vue
+++ application/resources/js/components/common/Footer.vue
@@ -1,33 +1,24 @@
 <template>
-  <v-footer class="bg-grey-lighten-1">
-    <v-row justify="center" no-gutters>
-      <v-btn
-        v-for="link in links"
-        :key="link.title"
-        class="mx-2"
-        color="white"
-        rounded="xl"
-        variant="text"
-        v-scroll-to="link.to"
-      >
-        {{ link.title }}
-      </v-btn>
-      <v-col class="text-center mt-4" cols="12">
-        {{ new Date().getFullYear() }} — About Me
-      </v-col>
-    </v-row>
-  </v-footer>
+  <div>
+    <v-footer>
+      <v-container>
+        <v-row justify="end" no-gutters>
+          <v-btn color="black" icon="mdi-chevron-up" @click="scrollWindow()" />
+        </v-row>
+      </v-container>
+    </v-footer>
+  </div>
 </template>
 
 <script>
 export default {
-  data: () => ({
-  links: [
-      {title:"About Me", to:"#aboutme"},
-      {title:"Skill Set", to:"#skill"},
-      {title:"Experience", to:"#experience"},
-      {title:"Release Note", to:"#release_note"},
-    ]
-  }),
+  methods: {
+    scrollWindow() {
+      window.scroll({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+  },
 };
 </script>
